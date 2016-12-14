755358d5d12e408059fb0aa7ae602ff9ddbac614
ANDRAS SZABO <szabo.andrew@yahoo.com>
2016-12-14 19:07:09 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(int argc, const char* argv[]) {
+
+
+	for (int i = 0; i<argc; i++)
+	{
+		cout << argv[i] << endl;
+	}
+
+
+	//how to make exefile in visual studio
+	//sleep(10000);
+	system("pause");
+	return 0;
+}
